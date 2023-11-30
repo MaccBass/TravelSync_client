a2d4acb29e6fa0088166a1b28fed89d2a2037266
dlekdms2 <dlekdms_02@naver.com>
2023-11-30 19:00:35 +0900

    feat: tour/plan API 연동 중

--- lib/widget/loginpage.dart
+++ lib/widget/loginpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travelsync_client/widget/loginWidgetGroup.dart';
+//import '../widget/loginWidgetGroup.dart';
 
 class Loginpage extends StatelessWidget {
   const Loginpage({super.key});
